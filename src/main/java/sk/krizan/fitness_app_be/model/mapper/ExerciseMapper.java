c839c8ace98383fe9da5d0ee23720bd31778c740
krizek79 <krizan.matej79@gmail.com>
2023-09-03 21:40:15 +0200

    some mappers

---
+++ src/main/java/sk/krizan/fitness_app_be/model/mapper/ExerciseMapper.java
@@ -0,0 +1,10 @@
+package sk.krizan.fitness_app_be.model.mapper;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ExerciseMapper {
+}

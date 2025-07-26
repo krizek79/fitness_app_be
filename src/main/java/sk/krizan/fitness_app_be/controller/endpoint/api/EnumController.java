7af5919b5789fbaf8052afee78c8ac0bf23ac220
Matej Križan <krizan.matej79@gmail.com>
2025-07-26 19:34:23 +0200

    api definition for EnumController

---
+++ src/main/java/sk/krizan/fitness_app_be/controller/endpoint/api/EnumController.java
@@ -0,0 +1,132 @@
+package sk.krizan.fitness_app_be.controller.endpoint.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sk.krizan.fitness_app_be.controller.response.EnumResponse;
+import sk.krizan.fitness_app_be.controller.response.ExceptionResponse;
+
+import java.util.List;
+
+@Tag(
+        name = "Enum",
+        description = "Provides available values for enums"
+)
+@RequestMapping("enums")
+public interface EnumController {
+
+    @Operation(
+            summary = "Get workout levels",
+            description = "Returns a list of possible workout levels.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of workout levels",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = EnumResponse.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+            }
+    )
+    @GetMapping("workout-levels")
+    List<EnumResponse> getWorkoutLevels();
+
+    @Operation(
+            summary = "Get muscle groups",
+            description = "Returns a list of available muscle groups.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of muscle groups",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = EnumResponse.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+            }
+    )
+    @GetMapping("muscle-groups")
+    List<EnumResponse> getMuscleGroups();
+
+    @Operation(
+            summary = "Get weight units",
+            description = "Returns a list of supported weight units.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of weight units",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = EnumResponse.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+            }
+    )
+    @GetMapping("weight-units")
+    List<EnumResponse> getWeightUnits();
+
+    @Operation(
+            summary = "Get workout exercise types",
+            description = "Returns a list of types of workout exercises.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of workout exercise types.",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = EnumResponse.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+            }
+    )
+    @GetMapping("workout-exercise-types")
+    List<EnumResponse> getWorkoutExerciseTypes();
+
+    @Operation(
+            summary = "Get workout exercise set types",
+            description = "Returns a list of set types for workout exercises.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of workout exercise set types",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = EnumResponse.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+            }
+    )
+    @GetMapping("workout-exercise-set-types")
+    List<EnumResponse> getWorkoutExerciseSetTypes();
+}
--- src/main/java/sk/krizan/fitness_app_be/controller/endpoint/impl/EnumController.java
+++ src/main/java/sk/krizan/fitness_app_be/controller/endpoint/impl/EnumController.java
@@ -2,48 +2,44 @@ package sk.krizan.fitness_app_be.controller.endpoint.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sk.krizan.fitness_app_be.controller.response.EnumResponse;
 import sk.krizan.fitness_app_be.service.api.EnumService;
 
 import java.util.List;
 
-//  TODO
 @RestController
-@RequestMapping("enums")
 @RequiredArgsConstructor
-public class EnumController {
+public class EnumController implements sk.krizan.fitness_app_be.controller.endpoint.api.EnumController {
 
     private final EnumService enumService;
 
-    @GetMapping("workout-levels")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    @Override
     public List<EnumResponse> getWorkoutLevels() {
         return enumService.getWorkoutLevels();
     }
 
-    @GetMapping("muscle-groups")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    @Override
     public List<EnumResponse> getMuscleGroups() {
         return enumService.getMuscleGroups();
     }
 
-    @GetMapping("weight-units")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    @Override
     public List<EnumResponse> getWeightUnits() {
         return enumService.getWeightUnits();
     }
 
-    @GetMapping("workout-exercise-types")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    @Override
     public List<EnumResponse> getWorkoutExerciseTypes() {
         return enumService.getWorkoutExerciseTypes();
     }
 
-    @GetMapping("workout-exercise-set-types")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    @Override
     public List<EnumResponse> getWorkoutExerciseSetTypes() {
         return enumService.getWorkoutExerciseSetTypes();
     }
